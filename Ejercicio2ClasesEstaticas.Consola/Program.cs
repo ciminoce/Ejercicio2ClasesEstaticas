75468af90476a1bd0af1e95c5f9189fd00e7c7e5
ciminoce <ciminoce@yahoo.com.ar>
2023-06-22 11:14:10 -0300

    Agregar archivos de proyecto.

---
+++ Ejercicio2ClasesEstaticas.Consola/Program.cs
@@ -0,0 +1,21 @@
+﻿using Ejercicio2ClasesEstaticas.Entidades;
+using System;
+
+namespace Ejercicio2ClasesEstaticas.Consola
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese un nro entero:");
+            var nro=int.Parse(Console.ReadLine());
+            Console.WriteLine($"El equivalente en binario de {nro} es {Conversor.ConvertirDeDecimalABinario(nro)}");
+
+           
+            Console.Write("Ingrese un nro binario:");
+            var nroBinario=int.Parse(Console.ReadLine());
+            Console.WriteLine($"El equivalente en decimal de {nroBinario} es {Conversor.ConvertirDeBinarioADecimal(nroBinario)}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Ejercicio2ClasesEstaticas.Entidades/Conversor.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Ejercicio2ClasesEstaticas.Entidades
+{
+    public static class Conversor
+    {
+       /// <summary>
+       /// Método estático para convertir un nro entero decimal a binario
+       /// </summary>
+       /// <param name="nro">Número entero a convertir</param>
+       /// <returns>El valor en binario del nro ingresado</returns>
+        public static int ConvertirDeDecimalABinario(int nro)
+        {
+            int x = 1;
+            int binario = 0;
+            while (nro >= 1)
+            {
+                if (nro % 2 == 1)
+                {
+                    binario = binario + x;
+                }
+                nro = Convert.ToInt32(Math.Truncate((double)nro / 2));
+                x = x * 10;
+            }
+            return binario;
+        }
+
+        public static int ConvertirDeBinarioADecimal(int binario)
+        {
+            int e = 1;
+            int nroDecimal = 0;
+            int Y;
+            if (binario ==0)
+            {
+                return nroDecimal;
+            }
+            while (binario>0)
+            {
+                Y = binario % 10;
+                binario = binario / 10;
+                nroDecimal = nroDecimal + (Y * e);
+                e = 2 * e;
+            }
+            return nroDecimal;
+        }
+    }
+}
